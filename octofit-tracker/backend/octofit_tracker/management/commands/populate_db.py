8e798a991da1e8999d9adae33a57a9974ba6ac53
karthikduggirala87 <51411212+karthikduggirala87@users.noreply.github.com>
2026-03-08 14:26:20 +0000

    Add explicit primary keys and valid id fields for all collections

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -13,27 +13,27 @@ class Command(BaseCommand):
         Workout.objects.all().delete()
 
         # Create teams
-        marvel = Team.objects.create(name='marvel', members=['Iron Man', 'Captain America', 'Thor'])
-        dc = Team.objects.create(name='dc', members=['Superman', 'Batman', 'Wonder Woman'])
+        marvel = Team.objects.create(id=1, name='marvel', members=['Iron Man', 'Captain America', 'Thor'])
+        dc = Team.objects.create(id=2, name='dc', members=['Superman', 'Batman', 'Wonder Woman'])
 
         # Create users
-        User.objects.create(email='ironman@marvel.com', name='Iron Man', team='marvel')
-        User.objects.create(email='cap@marvel.com', name='Captain America', team='marvel')
-        User.objects.create(email='thor@marvel.com', name='Thor', team='marvel')
-        User.objects.create(email='superman@dc.com', name='Superman', team='dc')
-        User.objects.create(email='batman@dc.com', name='Batman', team='dc')
-        User.objects.create(email='wonderwoman@dc.com', name='Wonder Woman', team='dc')
+        User.objects.create(id=1, email='ironman@marvel.com', name='Iron Man', team='marvel')
+        User.objects.create(id=2, email='cap@marvel.com', name='Captain America', team='marvel')
+        User.objects.create(id=3, email='thor@marvel.com', name='Thor', team='marvel')
+        User.objects.create(id=4, email='superman@dc.com', name='Superman', team='dc')
+        User.objects.create(id=5, email='batman@dc.com', name='Batman', team='dc')
+        User.objects.create(id=6, email='wonderwoman@dc.com', name='Wonder Woman', team='dc')
 
         # Create activities
-        Activity.objects.create(user='Iron Man', type='run', duration=30, date='2026-03-08')
-        Activity.objects.create(user='Batman', type='cycle', duration=45, date='2026-03-08')
+        Activity.objects.create(id=1, user='Iron Man', type='run', duration=30, date='2026-03-08')
+        Activity.objects.create(id=2, user='Batman', type='cycle', duration=45, date='2026-03-08')
 
         # Create leaderboard
-        Leaderboard.objects.create(team='marvel', points=150)
-        Leaderboard.objects.create(team='dc', points=120)
+        Leaderboard.objects.create(id=1, team='marvel', points=150)
+        Leaderboard.objects.create(id=2, team='dc', points=120)
 
         # Create workouts
-        Workout.objects.create(name='Pushups', description='Do 20 pushups', suggested_for='marvel')
-        Workout.objects.create(name='Situps', description='Do 30 situps', suggested_for='dc')
+        Workout.objects.create(id=1, name='Pushups', description='Do 20 pushups', suggested_for='marvel')
+        Workout.objects.create(id=2, name='Situps', description='Do 30 situps', suggested_for='dc')
 
         self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data'))
--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,6 +1,7 @@
 from djongo import models
 
 class User(models.Model):
+    id = models.AutoField(primary_key=True)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
     team = models.CharField(max_length=50)
@@ -8,12 +9,14 @@ class User(models.Model):
         db_table = 'users'
 
 class Team(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, unique=True)
     members = models.JSONField(default=list)
     class Meta:
         db_table = 'teams'
 
 class Activity(models.Model):
+    id = models.AutoField(primary_key=True)
     user = models.CharField(max_length=100)
     type = models.CharField(max_length=50)
     duration = models.IntegerField()
@@ -22,12 +25,14 @@ class Activity(models.Model):
         db_table = 'activities'
 
 class Leaderboard(models.Model):
+    id = models.AutoField(primary_key=True)
     team = models.CharField(max_length=50)
     points = models.IntegerField()
     class Meta:
         db_table = 'leaderboard'
 
 class Workout(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
     suggested_for = models.CharField(max_length=50)
